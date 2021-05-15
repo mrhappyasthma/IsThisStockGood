5bf4a85fbc048346464b202dd1362f1e2a0fc406
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-05-14 18:30:38 -0700

    Add starter code for YahooFinanceQuoteSummary

--- src/YahooFinance.py
+++ src/YahooFinance.py
@@ -65,3 +65,63 @@ class YahooFinanceAnalysis:
         percentage = YahooFinanceAnalysis._parseNextPercentage(tree_iterator)
         self.five_year_growth_rate = percentage.rstrip("%") if percentage else None
     return True if self.five_year_growth_rate else False
+
+
+## (unofficial) API documentation: https://observablehq.com/@stroked/yahoofinance
+class YahooFinanceQuoteSummary:
+  # Expects the ticker symbol as the first format string, and a comma-separated list
+  # of `QuotesummaryModules` strings for the second argument.
+  URL_TEMPLATE = 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/{}?modules={}'
+
+  # A list of modules that can be used inside of `QUOTE_SUMMARY_URL_TEMPLATE`.
+  # These should be passed as a comma-separated list.
+  MODULES = {
+    "assetProfile": "assetProfile",  # Company info/background
+    "incomeStatementHistory": "incomeStatementHistory",
+    "incomeStatementHistoryQuarterly": "incomeStatementHistoryQuarterly",
+    "balanceSheetHistory": "balanceSheetHistory",  # Current cash/equivalents
+    "balanceSheetHistoryQuarterly": "balanceSheetHistoryQuarterly",
+    "cashFlowStatementHistory": "cashFlowStatementHistory",
+    "cashFlowStatementHistoryQuarterly": "cashFlowStatementHistoryQuarterly",
+    "defaultKeyStatistics": "defaultKeyStatistics",
+    "financialData": "financialData",
+    "calendarEvents": "calendarEvents",  # Contains ex-dividend date
+    "secFilings": "secFilings",  # SEC filing links
+    "recommendationTrend": "recommendationTrend",
+    "upgradeDowngradeHistory": "upgradeDowngradeHistory",
+    "institutionOwnership": "institutionOwnership",
+    "fundOwnership": "fundOwnership",
+    "majorDirectHolders": "majorDirectHolders",
+    "majorHoldersBreakdown": "majorHoldersBreakdown",
+    "insiderTransactions": "insiderTransactions",
+    "insiderHolders": "insiderHolders",
+    "netSharePurchaseActivity": "netSharePurchaseActivity",
+    "netSharePurchaseActivity": "earnings",
+    "earningsHistory": "earningsHistory",
+    "earningsTrend": "earningsTrend",
+    "industryTrend": "industryTrend",
+    "indexTrend": "indexTrend",
+    "sectorTrend": "sectorTrend"
+  }
+
+  @classmethod
+  def _construct_url(cls, ticker_symbol, modules):
+    modulesString = cls._construct_modules_string(modules)
+    return cls.URL_TEMPLATE.format(ticker_symbol, modulesString)
+
+  # A helper method to return a formatted modules string.
+  @classmethod
+  def _construct_modules_string(cls, modules):
+    modulesString = modules[0]
+    for index, module in enumerate(modules, start=1):
+      modulesString = modulesString + ',' + module
+    return modulesString
+
+  # Accepts the ticker symbol followed by a list of
+  # `YahooFinanceQuoteSummary.MODULES`.
+  def __init__(self, ticker_symbol, modules):
+    self.ticker_symbol = ticker_symbol
+    self.modules = modules
+    self.url = YahooFinanceQuote._construct_url(ticker_symbol, modules)
+
+  ## TODO: Add parsing for relevant modules.
