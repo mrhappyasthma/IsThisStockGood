c53f1a20101565d8b444dba95cfa5a3f041fda32
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-05-14 18:26:49 -0700

    Fix a parsing error.

--- src/YahooFinance.py
+++ src/YahooFinance.py
@@ -36,15 +36,20 @@ class YahooFinanceAnalysis:
 
   @classmethod
   def _isPercentage(cls, text):
+    if not isinstance(text, str):
+      return False
     match = re.match('(\d+(\.\d+)?%)', text)
     return match != None
 
   @classmethod
   def _parseNextPercentage(cls, iterator):
-    node = None
-    while node is None or not cls._isPercentage(node.text):
-      node = next(iterator)
-    return node.text
+    try:
+      node = None
+      while node is None or not cls._isPercentage(node.text):
+        node = next(iterator)
+      return node.text
+    except:  # End of iteration
+      return None
 
   def __init__(self, ticker_symbol):
     self.ticker_symbol = ticker_symbol
@@ -58,5 +63,5 @@ class YahooFinanceAnalysis:
       text = element.text
       if text == 'Next 5 Years (per annum)':
         percentage = YahooFinanceAnalysis._parseNextPercentage(tree_iterator)
-        self.five_year_growth_rate = percentage.rstrip("%")
+        self.five_year_growth_rate = percentage.rstrip("%") if percentage else None
     return True if self.five_year_growth_rate else False
