016d9442dc78258bd66db065ee067ca64ccea17e
kocielnik <patryk@kocielnik.pl>
2024-07-01 21:12:00 +0200

    #28: Cleaning up a little to enable unit-testing API code. (#78)
    * Add an API test.
    
    * Try to enable GitHub Actions.
    
    * Try to remove the Werkzeug warning.
    
    * Get a real quote.
    
    * Rename package to src for now.
    
    While pyproject.toml must contain the package name, using a name other
    than "src" would require cascading changes to the submodules.
    
    Let's leave that for a later stage, if required.
    
    * Allow Python 3.10.
    
    * Allow Python ^3.8.
    
    Recent Flask requires ^3.8.
    
    * Skip checking for requirements.txt (extra step).
    
    * Remove requirements.txt.
    
    * Add PyTest as a plain dependency.
    
    * Specify Mark as the author.
    
    * Refactor: Move server logic from main.py to src/server.py.
    
    * Debug: Print current values.
    
    * Remove a debug print.
    
    * Remove CircleCI config.
    
    Configured GitHub Actions instead.
    
    If that isn't equivalent, we can revert.
    
    * Untrack .envrc.
    
    * Remove .envrc.
    
    * Add PyLint exclusions for most common warnings.
    
    These specific warnings were so numerous they obscured the code.
    
    Before anyone gets to cleaning these warnings [1], let's disable them
    for now just to make reading the code easier.
    
    [1]: *If* they should ever be cleaned at all.
    
    * Move test_api to tests/.
    
    * Remove unneeded mock.

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
-import logging
-from datetime import date
-from flask import Flask, request, render_template
 from src.DataFetcher import fetchDataForTickerSymbol
-
-logger = logging.getLogger("IsThisStockGood")
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.WARNING)
-
-h_format = logging.Formatter('%(name)s - %(levelname)s : %(message)s')
-handler.setFormatter(h_format)
-
-logger.addHandler(handler)
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def homepage():
-  if request.environ['HTTP_HOST'].endswith('.appspot.com'):  #Redirect the appspot url to the custom url
-    return '<meta http-equiv="refresh" content="0; url=https://isthisstockgood.com" />'
-
-  template_values = {
-    'page_title' : "Is This Stock Good?",
-    'current_year' : date.today().year,
-  }
-  return render_template('home.html', **template_values)
-
-
-@app.route('/search', methods=['POST'])
-def search():
-  if request.environ['HTTP_HOST'].endswith('.appspot.com'):  #Redirect the appspot url to the custom url
-    return '<meta http-equiv="refresh" content="0; url=http://isthisstockgood.com" />'
-
-  ticker = request.values.get('ticker')
-  template_values = fetchDataForTickerSymbol(ticker)
-  if not template_values:
-    return render_template('json/error.json', **{'error' : 'Invalid ticker symbol'})
-  return render_template('json/stock_data.json', **template_values)
+from src.server import create_app
 
 
 if __name__ == '__main__':
+  app = create_app(fetchDataForTickerSymbol)
   app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ tests/test_api.py
@@ -0,0 +1,24 @@
+import json
+from src.server import create_app
+from src.DataFetcher import fetchDataForTickerSymbol
+
+
+def test_import_app():
+    app = create_app(fetchDataForTickerSymbol)
+
+    with app.test_client() as test_client:
+        test_client = app.test_client()
+        res = test_client.get('/api')
+        data = res.text
+        assert json.loads(data) == {}
+        assert res.status_code == 200
+
+def test_get_data():
+    app = create_app(fetchDataForTickerSymbol)
+
+    with app.test_client() as test_client:
+        test_client = app.test_client()
+        res = test_client.get('/api/ticker/nvda')
+        data = res.text
+        assert json.loads(data)['debt_payoff_time'] == 0
+        assert res.status_code == 200
