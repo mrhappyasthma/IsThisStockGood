db6eb1c3b490045864581d75947af1e5bfc04048
Patryk Kocielnik <patryk@kocielnik.pl>
2024-07-22 16:04:59 +0200

    Use a dataclass to represent company info.

--- isthisstockgood/Active/MSNMoney.py
+++ isthisstockgood/Active/MSNMoney.py
@@ -1,5 +1,33 @@
+from dataclasses import dataclass
 import json
 import isthisstockgood.RuleOneInvestingCalculations as RuleOne
+
+@dataclass
+class CompanyInfo:
+    ticker_symbol: str
+    name: str
+    description: str
+    industry: str
+    current_price: float
+    average_volume: float
+    market_cap: float
+    shares_outstanding: int
+    pe_high: float
+    pe_low: float
+    roic: float
+    roic_averages: [float]
+    equity: float
+    equity_growth_rates: [float]
+    free_cash_flow: float
+    free_cash_flow_growth_rates: [float]
+    revenue: float
+    revenue_growth_rates: [float]
+    eps: float
+    eps_growth_rates: [float]
+    debt_equity_ratio: float
+    last_year_net_income: float
+    total_debt: float
+
 
 class MSNMoney:
   # This key appears to be fixed? So we can use it for now /shrug
