0f9f8c2d6a7040948caa676c5b23f4fd15fc4db4
mrhappyasthma <mrhappyasthma@gmail.com>
2025-01-23 14:51:59 -0800

    Move `app` to module scope to work with newer runtimes.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from isthisstockgood.DataFetcher import fetchDataForTickerSymbol
 from isthisstockgood.server import create_app
 
+# Expose `app` object at the module level, as expected by App Engine
+app = create_app(fetchDataForTickerSymbol)
 
 if __name__ == '__main__':
-  app = create_app(fetchDataForTickerSymbol)
   app.run(host='127.0.0.1', port=8080, debug=True)
